b24ceb21ff0365a429af3e97df36d4465643224d
Daniel Lord <daniel.lord@waterstons.com>
2018-01-19 16:33:39 +0000

    hey

---
+++ HackDayBot.Bot/Dialogs/RootDialog.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using JSONUtils;
+using System.Web.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace HackDayBot.Bot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            // calculate something for us to return
+            int length = (activity.Text ?? string.Empty).Length;
+
+            var test = await AskLuis(activity.Text);
+
+            // return our reply to the user
+            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task<string> AskLuis(string searchString)
+        {
+            Query query = new Query();
+
+            if(searchString != null)
+            {
+                await QueryLUIS(searchString);
+            }
+            return "";
+        }
+
+        private static async Task<LUIS> QueryLUIS(string Query)
+        {
+            LUIS LUISResult = new LUIS();
+            var LUISQuery = Uri.EscapeDataString(Query);
+            using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
+            {
+                // Get key values from the web.config
+                string LUIS_Url = WebConfigurationManager.AppSettings["LUIS_Url"];
+                string LUIS_Subscription_Key = WebConfigurationManager.AppSettings["LUIS_Subscription_Key"];
+                string RequestURI = String.Format("{0}?subscription-key={1}&q={2}",
+                    LUIS_Url, LUIS_Subscription_Key, LUISQuery);
+                System.Net.Http.HttpResponseMessage msg = await client.GetAsync(RequestURI);
+                if (msg.IsSuccessStatusCode)
+                {
+                    var JsonDataResponse = await msg.Content.ReadAsStringAsync();
+
+                    var data = JObject.Parse(JsonDataResponse);
+
+                    //var intent = data[intent];
+                    //var client = data[0].[entity];
+
+
+                    //extract client from JSON file.
+
+                    //work out column to query using intent
+                    //calling the appropriate method and passing the client.
+                    //work out the client it is for.
+
+
+
+
+
+                    LUISResult = JsonConvert.DeserializeObject<LUIS>(JsonDataResponse);
+                }
+            }
+            return LUISResult;
+        }
+    }
+}
---
+++ HackDayBot.Bot/Models/LUIS.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace JSONUtils
+{
+
+    public class TopScoringIntent
+    {
+        public string intent { get; set; }
+        public double score { get; set; }
+    }
+
+    public class Intent
+    {
+        public string intent { get; set; }
+        public double score { get; set; }
+    }
+
+    public class Resolution
+    {
+        public IList<string> values { get; set; }
+    }
+
+    public class Entity
+    {
+        public string entity { get; set; }
+        public string type { get; set; }
+        public int startIndex { get; set; }
+        public int endIndex { get; set; }
+        public Resolution resolution { get; set; }
+    }
+
+    public class LUIS
+    {
+        public string query { get; set; }
+        public TopScoringIntent topScoringIntent { get; set; }
+        public IList<Intent> intents { get; set; }
+        public IList<Entity> entities { get; set; }
+    }
+
+    public class Query
+    {
+        public string link { get; set; }
+    }
+
+
+}
