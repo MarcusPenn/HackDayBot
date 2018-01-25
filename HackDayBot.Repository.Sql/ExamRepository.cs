f768e43fc811ca988bd4a4a7d9e01c31cd1610c4
Marcus Penn <marcus.penn@waterstons.com>
2018-01-25 14:44:55 +0000

    Added simple exam repo

---
+++ HackDayBot.Repository.Sql/ExamRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace HackDayBot.Repository.Sql
+{
+    public class ExamRepository : DapperRepository
+    {
+        private const string TableName = "Exams";
+
+        public string GetExamName(int id)
+        {
+            var query = $"SELECT Name FROM {TableName} WHERE Id = '{id}'";
+
+            return ExecuteQuery(query);
+        }
+    }
+}
